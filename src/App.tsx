48c30450ba83aad7ed3c729b34df667593fbfaaf
de-scientist <gitaumark502@gmail.com>
2025-11-15 15:48:59 +0300

    import Services from "./components/Services";

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import React from "react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import About from "./components/About";
+import Services from "./components/Services";
 
 const App: React.FC = () => {
   return (
