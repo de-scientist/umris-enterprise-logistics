ecf30b460dcf4ab5b73e3961380e6ea46f016dad
de-scientist <gitaumark502@gmail.com>
2025-11-15 15:33:47 +0300

    export the footer function component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer: React.FC = () => (
+  <footer className="footer">
+    <p className="footer-text">
+      Built with passion by <a href="https://github.com/de-scientist" target="_blank">Mark Kinyanjui</a>
+    </p>
+  </footer>
+);
+
+export default Footer;
